053ea5596d68e5e008edb35e6d5a5978561b8660
吕佐康 <lvzk@techsure.com.cn>
2021-03-29 16:53:54 +0800

    删除没用的异常

--- src/main/java/codedriver/module/dashboard/exception/DashboardNameExistsException.java
+++ src/main/java/codedriver/module/dashboard/exception/DashboardNameExistsException.java
@@ -1,8 +1,8 @@
 package codedriver.module.dashboard.exception;
 
-import codedriver.framework.exception.core.ApiFieldValidRuntimeException;
+import codedriver.framework.exception.core.ApiRuntimeException;
 
-public class DashboardNameExistsException extends ApiFieldValidRuntimeException {
+public class DashboardNameExistsException extends ApiRuntimeException {
 	private static final long serialVersionUID = 4230545560652554738L;
 
 	public DashboardNameExistsException(String uuid) {
