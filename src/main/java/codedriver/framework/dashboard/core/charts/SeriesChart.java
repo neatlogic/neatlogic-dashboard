0f2b24f5c2dfd4e46680acb22900ca82ab09eba3
lvzk <lvzk@techsure.com.cn>
2022-02-17 18:52:50 +0800

    --task=1005942 --user=lvzk ITSM dashboard 分组条件:每天 https://www.tapd.cn/54247054/s/1208903

--- src/main/java/codedriver/framework/dashboard/core/charts/SeriesChart.java
+++ src/main/java/codedriver/framework/dashboard/core/charts/SeriesChart.java
@@ -18,7 +18,7 @@ public class SeriesChart extends DashboardChartBase {
 	public JSONObject getChartConfig() {
 		JSONObject charConfig = new JSONObject();
 		JSONObject showConfig = new JSONObject();
-		showConfig.put(DashboardShowConfig.AGGREGATE.getValue(),new DashboardShowConfigVo(DashboardShowConfig.AGGREGATE,JSONArray.parseArray("[{'value':'count','text':'计数','isDefault':1}]")));
+		showConfig.put(DashboardShowConfig.AGGREGATE.getValue(),new DashboardShowConfigVo(DashboardShowConfig.AGGREGATE,JSONArray.parseArray("[{'value':'sum','text':'总数','isDefault':1}]")));
 		showConfig.put(DashboardShowConfig.GROUPFIELD.getValue(),new DashboardShowConfigVo(DashboardShowConfig.GROUPFIELD,new JSONArray()));
 		showConfig.put(DashboardShowConfig.SUBGROUPFIELD.getValue(),new DashboardShowConfigVo(DashboardShowConfig.SUBGROUPFIELD,new JSONArray()));
 		showConfig.put(DashboardShowConfig.MAXGROUP.getValue(),new DashboardShowConfigVo(DashboardShowConfig.MAXGROUP,JSONArray.parseArray("[{'value':'10','text':'10','isDefault':1},{'value':'20','text':'20'}]")));
